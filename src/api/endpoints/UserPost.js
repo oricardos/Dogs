3aea6f5cd9e43f52c61034556ad876808042d185
oricardos <ricardo.rs95@gmail.com>
2021-08-30 23:37:21 -0300

    0803 API 2

---
+++ src/api/endpoints/UserPost.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const UserPost = () => {
+  const [username, setUsername] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={({ target }) => setUsername(target.value)}
+      />
+      <input
+        type="email"
+        placeholder="email"
+        value={email}
+        onChange={({ target }) => setEmail(target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={({ target }) => setPassword(target.value)}
+      />
+
+      <button>Enviar</button>
+    </div>
+  );
+};
+
+export default UserPost;
