0363c5c486d012db3a38af6b9eeed8da8d4e8d66
oricardos <ricardo.rs95@gmail.com>
2021-10-26 23:31:04 -0300

    0817 User Header

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router';
+import Feed from '../Feed/Feed';
+import UserHeader from './UserHeader';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
 
 const User = () => {
-  return <div>user</div>;
+  return (
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+      </Routes>
+    </section>
+  );
 };
 
 export default User;
