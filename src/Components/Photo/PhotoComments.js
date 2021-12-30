cedf02e8736098358f0644e5f950451b5a312ce3
oricardos <ricardo.rs95@gmail.com>
2021-12-29 23:58:35 -0300

    0822 Comentário Post

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { UserContext } from '../../UserContext';
 
 const PhotoComments = () => {
+  const { login } = React.useContext(UserContext);
   return <div></div>;
 };
 
---
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PhotoCommentsForm = () => {
+  return <div></div>;
+};
+
+export default PhotoCommentsForm;
