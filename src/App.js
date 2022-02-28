8385c4080950a49401c6a9dab49e656089460f50
oricardos <ricardo.rs95@gmail.com>
2022-02-28 16:12:16 -0300

    feat: Footer
    Add footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import ProtectedRoute from './Components/Helper/ProtectedRoute';
-import Home from './Components/Home';
-import Login from './Components/Login/Login';
-import User from './Components/User/User';
-import { UserStorage } from './UserContext';
-import Photo from './Components/Photo/Photo'
-import UserProfile from './Components/User/UserProfile';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import User from "./Components/User/User";
+import { UserStorage } from "./UserContext";
+import Photo from "./Components/Photo/Photo";
+import UserProfile from "./Components/User/UserProfile";
 
 function App() {
   return (
@@ -17,13 +17,15 @@ function App() {
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="login/*" element={<Login />} />
-            <ProtectedRoute path="conta/*" element={<User />} />
-            <Route path="foto/:id" element={<Photo />} />
-            <Route path="perfil/:user" element={<UserProfile />} />
-          </Routes>
+          <main className="AppBody">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <ProtectedRoute path="conta/*" element={<User />} />
+              <Route path="foto/:id" element={<Photo />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
+            </Routes>
+          </main>
           <Footer />
         </UserStorage>
       </BrowserRouter>
