d3b866b68c4a7a50c021e64cd62bae5e465e5cd3
oricardos <ricardo.rs95@gmail.com>
2022-02-28 23:55:09 -0300

    feat: lazy
    Usando lazy para carregar biblioteca

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -3,8 +3,9 @@ import Head from '../Helper/Head';
 import useFetch from '../../Hooks/useFetch';
 import { STATS_GET } from '../../api';
 import Loading from '../Helper/Loading'
-import UserStatsGraphs from './UserStatsGraphs';
 import Error from '../Helper/Error';
+
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'))
 
 const UserStats = () => {
   const {data, error, loading, request} = useFetch();
@@ -20,10 +21,11 @@ const UserStats = () => {
   if(loading) return <Loading />
   if(error) return <Error error={error} />
   if(data)
-    return <div>
-      <Head title="Estatísticas" />
-      <UserStatsGraphs data={data}/>
-    </div>;
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data}/>
+      </React.Suspense>);
     else return null;
 };
 
